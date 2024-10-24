f8098a21befccb1b273202b71cb128afeb606c59
Jad Assaf <jad.a.assaf@gmail.com>
2024-10-24 21:37:24 +0300

    Test

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -59,69 +59,6 @@ async function fetchCollectionsByHandles(context, handles) {
 }
 
 /**
- * Load recommended products.
- */
-function loadDeferredData({ context }) {
-  const recommendedProducts = context.storefront
-    .query(RECOMMENDED_PRODUCTS_QUERY)
-    .catch((error) => {
-      console.error(error);
-      return null;
-    });
-
-  return { recommendedProducts };
-}
-
-export default function Homepage() {
-  const data = useLoaderData();
-
-  return (
-    <div className="home">
-      <CollectionDisplay collections={data.collections} />
-      <RecommendedProducts products={data.recommendedProducts} />
-    </div>
-  );
-}
-
-/**
- * Recommended products component.
- */
-function RecommendedProducts({ products }) {
-  return (
-    <div className="recommended-products">
-      <h2>Recommended Products</h2>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Await resolve={products}>
-          {(response) => (
-            <div className="recommended-products-grid">
-              {response
-                ? response.products.nodes.map((product) => (
-                  <Link
-                    key={product.id}
-                    className="recommended-product"
-                    to={`/products/${product.handle}`}
-                  >
-                    <Image
-                      data={product.images.nodes[0]}
-                      aspectRatio="1/1"
-                      sizes="(min-width: 45em) 20vw, 50vw"
-                    />
-                    <h4>{product.title}</h4>
-                    <small>
-                      <Money data={product.priceRange.minVariantPrice} />
-                    </small>
-                  </Link>
-                ))
-                : null}
-            </div>
-          )}
-        </Await>
-      </Suspense>
-    </div>
-  );
-}
-
-/**
  * GraphQL query to fetch a single collection by handle.
  */
 const GET_COLLECTION_BY_HANDLE_QUERY = `#graphql
@@ -153,43 +90,8 @@ const GET_COLLECTION_BY_HANDLE_QUERY = `#graphql
   }
 `;
 
-/**
- * GraphQL query to fetch recommended products.
- */
-const RECOMMENDED_PRODUCTS_QUERY = `#graphql
-  fragment RecommendedProduct on Product {
-    id
-    title
-    handle
-    priceRange {
-      minVariantPrice {
-        amount
-        currencyCode
-      }
-    }
-    images(first: 1) {
-      nodes {
-        id
-        url
-        altText
-        width
-        height
-      }
-    }
-  }
-
-  query RecommendedProducts($country: CountryCode, $language: LanguageCode)
-    @inContext(country: $country, language: $language) {
-    products(first: 4, sortKey: UPDATED_AT, reverse: true) {
-      nodes {
-        ...RecommendedProduct
-      }
-    }
-  }
-`;
 
 /** @typedef {import('@shopify/remix-oxygen').LoaderFunctionArgs} LoaderFunctionArgs */
 /** @template T @typedef {import('@remix-run/react').MetaFunction<T>} MetaFunction */
 /** @typedef {import('storefrontapi.generated').FeaturedCollectionFragment} FeaturedCollectionFragment */
-/** @typedef {import('storefrontapi.generated').RecommendedProductsQuery} RecommendedProductsQuery */
 /** @typedef {import('@shopify/remix-oxygen').SerializeFrom<typeof loader>} LoaderReturnData */
