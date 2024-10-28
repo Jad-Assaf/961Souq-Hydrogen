06c7b9104b91c39d9e3fb8cfadee3e2616e5d4d3
Jad Assaf <jad.a.assaf@gmail.com>
2024-10-28 12:59:00 +0200

    Test

--- app/components/CollectionDisplay.jsx
+++ app/components/CollectionDisplay.jsx
@@ -21,7 +21,7 @@ export function CollectionDisplay({ collections, images }) {
                     </div>
                     {images[index] && (  // Ensure the image exists
                         <div className="row-image">
-                            <AnimatedImage src={images[index]} alt={`Collection ${index + 1}`} loading='lazy' width="750px" height="500px" />
+                            <AnimatedImage src={images[index]} alt={`Collection ${index + 1}`} loading='lazy'/>
                         </div>
                     )}
                 </div>
