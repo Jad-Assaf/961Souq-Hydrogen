6c4fa445fa471c864643fb390c9c8fc69056d6f9
Jad Assaf <jad.a.assaf@gmail.com>
2024-10-29 13:41:59 +0200

    Test

--- app/components/CollectionSlider.jsx
+++ app/components/CollectionSlider.jsx
@@ -54,8 +54,11 @@ export default function CollectionSlider() {
                         className="category-container"
                     >
                         <img
-                            src={collection.image.src}
-                            alt={collection.image.altText || collection.title}
+                            src={
+                                collection.image?.src ||
+                                'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/fallback-image.jpg'
+                            }
+                            alt={collection.image?.altText || collection.title}
                             className="category-image"
                         />
                         <div className="category-title">{collection.title}</div>
