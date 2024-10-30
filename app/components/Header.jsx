5d70166575cd510d0e8272c6c5ce13f2db272e72
Jad Assaf <jad.a.assaf@gmail.com>
2024-10-30 13:23:05 +0200

    Test

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -23,8 +23,8 @@ export function Header({ header, isLoggedIn, cart, publicStoreDomain }) {
               src="https://cdn.shopify.com/s/files/1/0552/0883/7292/files/logonew_1c8474b8-d0a3-4a90-a3fa-494ce9ca846f.jpg?v=1619452140"
               alt={`${shop.name} Logo`}
               className="header-logo"
-              width="200px"
-              height="100px"
+              width="150px"
+              height="auto"
             />
           </NavLink>
 
