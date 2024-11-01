d1c439b1c502aabcf1276821fe537bf9905d225a
Jad Assaf <jad.a.assaf@gmail.com>
2024-11-01 23:37:38 +0200

    Test

--- app/components/checkbox.tsx
+++ app/components/checkbox.tsx
@@ -1,6 +1,6 @@
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import * as React from "react";
-import { cn } from "~/lib/cn";
+import { cn } from "../lib/cn";
 import { IconCheck } from "./icons";
 
 interface CheckboxProps
