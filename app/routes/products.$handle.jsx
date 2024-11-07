322ad4bc5594051a650dfc4d0500eb049444f0de
Jad Assaf <jad.a.assaf@gmail.com>
2024-11-07 13:22:23 +0200

    Test

--- app/routes/products.$handle.jsx
+++ app/routes/products.$handle.jsx
@@ -122,27 +122,26 @@ export default function Product() {
             </Await>
           </Suspense>
         </div>
-      <br />
-        <div className="ProductPageBottom">
-          <p><strong>Description</strong></p>
-          <div dangerouslySetInnerHTML={{ __html: descriptionHtml }} />
-            <Analytics.ProductView
-              data={{
-                products: [
-                  {
-                    id: product.id,
-                    title: product.title,
-                    price: selectedVariant?.price.amount || '0',
-                    vendor: product.vendor,
-                    variantId: selectedVariant?.id || '',
-                    variantTitle: selectedVariant?.title || '',
-                    quantity: 1,
-                  },
-                ],
-              }}
-            />
-          </div>
-        </div>
+    </div>
+      <div className="ProductPageBottom">
+        <p><strong>Description</strong></p>
+        <div dangerouslySetInnerHTML={{ __html: descriptionHtml }} />
+        <Analytics.ProductView
+          data={{
+            products: [
+              {
+                id: product.id,
+                title: product.title,
+                price: selectedVariant?.price.amount || '0',
+                vendor: product.vendor,
+                variantId: selectedVariant?.id || '',
+                variantTitle: selectedVariant?.title || '',
+                quantity: 1,
+              },
+            ],
+          }}
+        />
+      </div>
     </div>
   );
 }
