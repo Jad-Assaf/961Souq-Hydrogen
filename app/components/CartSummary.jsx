95ea89bc0884b6e6f8b5994481295d079993e4ee
Jad Assaf <jad.a.assaf@gmail.com>
2024-11-11 14:07:43 +0200

    Test

--- app/components/CartSummary.jsx
+++ app/components/CartSummary.jsx
@@ -10,9 +10,9 @@ export function CartSummary({cart, layout}) {
 
   return (
     <div aria-labelledby="cart-summary" className={className}>
-      <h4>Totals</h4>
+      <h4>Subtotal</h4>
       <dl className="cart-subtotal">
-        <dt>Subtotal</dt>
+        {/* <dt>Subtotal</dt> */}
         <dd>
           {cart.cost?.subtotalAmount?.amount ? (
             <Money data={cart.cost?.subtotalAmount} />
@@ -21,8 +21,8 @@ export function CartSummary({cart, layout}) {
           )}
         </dd>
       </dl>
-      <CartDiscounts discountCodes={cart.discountCodes} />
-      <CartGiftCard giftCardCodes={cart.appliedGiftCards} />
+      {/* <CartDiscounts discountCodes={cart.discountCodes} /> */}
+      {/* <CartGiftCard giftCardCodes={cart.appliedGiftCards} /> */}
       <CartCheckoutActions checkoutUrl={cart.checkoutUrl} />
     </div>
   );
