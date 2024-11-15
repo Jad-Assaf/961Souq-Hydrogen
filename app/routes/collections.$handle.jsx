da9eead1d995b9fe53d447ce667784186a8768fd
Jad Assaf <jad.a.assaf@gmail.com>
2024-11-15 12:11:11 +0200

    Test

--- app/routes/collections.$handle.jsx
+++ app/routes/collections.$handle.jsx
@@ -378,12 +378,13 @@ export function ProductItem({ product, index, numberInRow }) {
           }
         }}
       >
-        <Link key={product.id} prefetch="intent" to={variantUrl}>
+        <Link key={product.id} prefetch="intent" to={variantUrl} className='collection-product-link'>
           {product.featuredImage && isInView && (
             <motion.div
               initial={{ filter: 'blur(10px)', opacity: 0 }}
               animate={{ filter: isImageLoaded ? 'blur(0px)' : 'blur(10px)', opacity: 1 }}
               transition={{ duration: 0.5 }}
+              className='collection-product-image'
             >
               <Image
                 srcSet={`${product.featuredImage.url}?width=300&quality=30 300w,
