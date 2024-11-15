6c4ffe02081e2c8f912f00c2e65410b7d80647bc
Jad Assaf <jad.a.assaf@gmail.com>
2024-11-15 12:38:17 +0200

    Test

--- app/routes/collections.$handle.jsx
+++ app/routes/collections.$handle.jsx
@@ -412,13 +412,6 @@ export function ProductItem({ product, index, numberInRow }) {
             </div>
           </div>
         </Link>
-        </motion.div>
-        <motion.div
-          initial={{ filter: 'blur(10px)', opacity: 0 }}
-          animate={{ filter: isImageLoaded ? 'blur(0px)' : 'blur(10px)', opacity: 1 }}
-          transition={{ duration: 0.5 }}
-          className='collection-product-form'
-        >
         <ProductForm
           product={product}
           selectedVariant={selectedVariant}
