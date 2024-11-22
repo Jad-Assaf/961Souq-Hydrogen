7735f13f232f8c8cb9f4b031228e9251a5fb12be
Jad Assaf <jad.a.assaf@gmail.com>
2024-11-22 10:27:53 +0200

    Test

--- app/routes/contact._index.jsx
+++ app/routes/contact._index.jsx
@@ -12,11 +12,12 @@ export default function ContactUs() {
             <div className="map-container">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.1036075725224!2d35.573411244654345!3d33.90335299151777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151f3dfcf5060a0d%3A0xf692c306dec18a7d!2s961SOUQ.COM!5e1!3m2!1sen!2slb!4v1732207673920!5m2!1sen!2slb"
-                    width="400"
-                    height="300"
+                    width="100%"
+                    height="100%"
                     style={{ border: 0 }}
-                    allowFullScreen
+                    allowFullScreen=""
                     loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
                 ></iframe>
             </div>
             <p>
