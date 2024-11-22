6d98c24cdb674388970ce6311fa1d4004b431578
Jad Assaf <jad.a.assaf@gmail.com>
2024-11-22 11:28:59 +0200

    Test

--- app/routes/contact._index.jsx
+++ app/routes/contact._index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "../styles/Contact.css"
+import "../styles/Contact.css";
 
 export const meta = () => {
     return [{ title: 'Contact Us | Hydrogen Storefront' }];
@@ -9,16 +9,6 @@ export default function ContactUs() {
     return (
         <div className="contact-us-page">
             <h1>Contact Us</h1>
-            <div className="map-container">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.1036075725224!2d35.573411244654345!3d33.90335299151777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151f3dfcf5060a0d%3A0xf692c306dec18a7d!2s961SOUQ.COM!5e1!3m2!1sen!2slb!4v1732207673920!5m2!1sen!2slb"
-                    width="100%"
-                    height="100%"
-                    style={{ border: 0 }}
-                    allowFullScreen=""
-                    loading="lazy"
-                ></iframe>
-            </div>
             <p>
                 We’d love to hear from you! Feel free to reach out to us for any
                 inquiries, feedback, or support.
