f043111aa19c5a5954ce349469ebae31a4993806
Jad Assaf <jad.a.assaf@gmail.com>
2024-11-22 15:14:45 +0200

    Test

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -39,7 +39,7 @@ async function loadCriticalData({ context }) {
 
   // Hardcoded handles for product rows.
   const hardcodedHandles = [
-    'new-arrivals', 'laptops', 'apple-macbook', 'apple-iphone', 'apple-accessories', 'gaming-laptops', 'gaming-consoles', 'console-games', 'samsung-mobile-phones', 'google-pixel-phones', 'mobile-accessories', 'desktops', 'pc-parts'
+    'new-arrivals', 'laptops', 'apple-macbook', 'apple-iphone', 'apple-accessories', 'gaming-laptops', 'gaming-consoles', 'console-games', 'samsung-mobile-phones', 'google-pixel-phones', 'mobile-accessories', 'desktops', 'pc-parts', 'business-monitors'
   ];
 
   // Fetch collections for product rows.
