4343de6a738c7634103333a687a2b9b054600a6b
Jad Assaf <jad.a.assaf@gmail.com>
2024-11-22 11:15:22 +0200

    Test

--- app/routes/contact._index.jsx
+++ app/routes/contact._index.jsx
@@ -1,45 +1,20 @@
 import React from 'react';
 import "../styles/Contact.css";
-import { json } from '@shopify/remix-oxygen';
-import { createContentSecurityPolicy } from '@shopify/hydrogen';
 
 export const meta = () => {
     return [{ title: 'Contact Us | Hydrogen Storefront' }];
 };
 
-// Set CSP and nonce
-export function loader() {
-    const { header, nonce } = createContentSecurityPolicy({
-        defaultSrc: ["'self'", "https://cdn.shopify.com", "https://shopify.com"],
-        frameSrc: ["https://www.google.com", "https://maps.google.com"],
-        scriptSrc: ["'self'", "https://maps.googleapis.com"],
-    });
-
-    return json(
-        { nonce }, // Pass nonce as data for the component
-        {
-            headers: {
-                'Content-Security-Policy': header,
-            },
-        }
-    );
-}
-
-export default function ContactUs({ nonce }) {
+export default function ContactUs() {
     return (
         <div className="contact-us-page">
             <h1>Contact Us</h1>
             <div className="map-container">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.1036075725224!2d35.573411244654345!3d33.90335299151777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151f3dfcf5060a0d%3A0xf692c306dec18a7d!2s961SOUQ.COM!5e1!3m2!1sen!2slb!4v1732207673920!5m2!1sen!2slb"
-                    width="100%"
-                    height="100%"
-                    style={{ border: 0 }}
-                    allowFullScreen=""
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                    nonce={nonce} // Add nonce attribute to the iframe for CSP compliance
-                ></iframe>
+                <img
+                    src="https://maps.app.goo.gl/yHGwV7uzqzyyotKH6"
+                    alt="961SOUQ Location"
+                    style={{ width: "100%", height: "auto", border: 0 }}
+                />
             </div>
             <p>
                 We’d love to hear from you! Feel free to reach out to us for any
