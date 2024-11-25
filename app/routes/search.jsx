4a49c5a3fbbed0c20c65d19f6869bd8b6b72d351
Jad Assaf <jad.a.assaf@gmail.com>
2024-11-25 15:11:57 +0200

    Test

--- app/routes/search.jsx
+++ app/routes/search.jsx
@@ -1,14 +1,10 @@
-// app/routes/search.jsx
 import { json } from '@shopify/remix-oxygen';
-import { useLoaderData } from '@remix-run/react';
 
-export function loader() {
-  console.log("Loader function called"); // This should log
+export async function loader({ request, context }) {
+  console.log("Loader function called"); // Log to server console
+  const url = new URL(request.url);
+  console.log("Request URL:", request.url); // Log the request URL
+
+  // Example of returning a response
   return json({ message: "Loader is working!" });
-}
-
-export default function SearchPage() {
-  console.log("SearchPage component rendered"); // This should log
-  const data = useLoaderData();
-  return <div>{data.message}</div>;
 }
