9838bfd5981c3205a41032139d52a4128fad49f8
Jad Assaf <jad.a.assaf@gmail.com>
2024-11-25 17:56:40 +0200

    Test

--- app/components/RecentlyViewed.jsx
+++ app/components/RecentlyViewed.jsx
@@ -43,7 +43,7 @@ export default function RecentlyViewedProducts({ currentProductId }) {
     // Function to fetch products from the Shopify Storefront API
     async function fetchProducts(productIds) {
         const storefrontAccessToken = import.meta.env.VITE_PUBLIC_STOREFRONT_API_TOKEN;
-        const shopDomain = import.meta.env.VITE_PUBLIC_SHOPIFY_STORE_DOMAIN;
+        const shopDomain = 'https://961souqs.myshopify.com';
 
         const query = `
       query getProductsByIds($ids: [ID!]!) {
