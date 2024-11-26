6646416e7fcef2c98ae172d1fbdf96123210b148
Jad Assaf <jad.a.assaf@gmail.com>
2024-11-26 13:10:31 +0200

    Test

--- app/routes/collections.$handle.jsx
+++ app/routes/collections.$handle.jsx
@@ -422,7 +422,7 @@ const ProductItem = React.memo(({ product, index, numberInRow }) => {
           }
         }}
       >
-        <div>
+        <div style={display= 'flex'}>
           <Link key={product.id} prefetch="intent" to={variantUrl} className='collection-product-link'>
             {product.featuredImage && isInView && (
               <motion.div
