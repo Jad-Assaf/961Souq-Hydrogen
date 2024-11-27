8d724744b5f37ba71ec383cf0f4825aad56d14cf
Jad Assaf <jad.a.assaf@gmail.com>
2024-11-27 10:24:34 +0200

    Test

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -60,18 +60,18 @@ async function loadCriticalData({ context }) {
 
   // Hardcoded handles for product rows.
   const hardcodedHandles = [
-    'new-arrivals', 'laptops',
-    'apple-macbook', 'apple-iphone', 'apple-accessories',
-    'gaming-laptops', 'gaming-consoles', 'console-games',
-    'samsung-mobile-phones', 'google-pixel-phones', 'mobile-accessories',
-    'garmin-smart-watch', 'samsung-watches', 'fitness-bands',
-    'earbuds', 'speakers', 'surround-systems',
-    'desktops', 'pc-parts', 'business-monitors',
-    'action-cameras', 'cameras', 'surveillance-cameras',
+    'new-arrivals', 'laptops', 
+    'apple-macbook', 'apple-iphone', 'apple-accessories', 
+    'gaming-laptops', 'gaming-consoles', 'console-games', 
+    'samsung-mobile-phones', 'google-pixel-phones', 'mobile-accessories', 
+    'garmin-smart-watch', 'samsung-watches', 'fitness-bands', 
+    'earbuds', 'speakers', 'surround-systems', 
+    'desktops', 'pc-parts', 'business-monitors', 
+    'action-cameras', 'cameras', 'surveillance-cameras', 
     'kitchen-appliances', 'cleaning-devices', 'lighting', 'streaming-devices', 'smart-devices', 'health-beauty'
   ];
 
-  // Fetch collections for all handles
+  // Fetch collections for product rows.
   const collections = await fetchCollectionsByHandles(context, [...menuHandles, ...hardcodedHandles]);
 
   // Return menu along with other data
@@ -79,7 +79,6 @@ async function loadCriticalData({ context }) {
 }
 
 const brandsData = [
-  // ... (brand data continues here)
   { name: "Apple", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/apple.png?v=1648112715", link: "/collections/apple" },
   { name: "HP", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/hp.png?v=1648112715", link: "/collections/hp-products" },
   { name: "MSI", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/msi-logo.jpg?v=1712761894", link: "/collections/msi-products" },
@@ -99,7 +98,7 @@ const brandsData = [
   { name: "Lenovo", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/lenovo-logo.jpg?v=1712762549", link: "/collections/lenovo-products" },
   { name: "LG", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/lg-logo.jpg?v=1712762606", link: "/collections/lg-products" },
   { name: "Meta", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/meta-logo.jpg?v=1712762516", link: "/collections/meta-products" },
-  { name: "Ubiquiti", image: "https:// cdn.shopify.com/s/files/1/0552/0883/7292/files/ubuquiti-logo.jpg?v=1712761841", link: "/collections/ubiquiti-products" },
+  { name: "Ubiquiti", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/ubuquiti-logo.jpg?v=1712761841", link: "/collections/ubiquiti-products" },
   { name: "Philips", image: "https://cdn.shopify.com/s/files/1/0552/0883/7292/files/philips-logo.jpg?v=1712762630", link: "/collections/philips-products" },
 ];
 
@@ -116,93 +115,106 @@ async function fetchCollectionsByHandles(context, handles) {
 }
 
 export default function Homepage() {
-  const { banners, collections, menu } = useLoaderData();
+  const { banners, collections, sliderCollections, menu } = useLoaderData();
 
   const images = [
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/apple-products_29a11658-9601-44a9-b13a-9a52c10013be.jpg?v=1728311525',
-      link: '/collections/apple',
+      link: '/collections/apple', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/APPLE-IPHONE-16-wh.jpg?v=1728307748',
-      link: '/collections/apple-iphone',
+      link: '/collections/apple-iphone', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/ps5-banner.jpg?v=1728289818',
-      link: '/collections/sony-playstation',
+      link: '/collections/sony-playstation', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/ps-studios.jpg?v=1728486402',
-      link: '/collections/console-games',
+      link: '/collections/console-games', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/cmf-phone-1-banner-1.jpg?v=1727944715',
-      link: '/collections/nothing-phones',
+      link: '/collections/nothing-phones', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/samsung-s24.jpg?v=1732281967',
-      link: '/collections/samsung-mobile-phones',
+      link: '/collections/samsung-mobile-phones', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/samsung-watch-ultra.jpg?v=1732281967',
-      link: '/products/samsung-galaxy-watch-ultra',
+      link: '/products/samsung-galaxy-watch-ultra', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/garmin-banner.jpg?v=1727943839',
-      link: '/collections/garmin-smart-watch',
+      link: '/collections/garmin-smart-watch', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/jbl-eaubuds.jpg?v=1732284726',
-      link: '/collections/earbuds',
+      link: '/collections/earbuds', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/steelseries-speakers.jpg?v=1711034859',
-      link: '/collections/gaming-speakers',
+      link: '/collections/gaming-speakers', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/gaming-desktops.jpg?v=1732287092',
-      link: '/collections/gaming-desktops',
+      link: '/collections/gaming-desktops', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/gaming-monitors_6069e5a5-45c8-4ff2-8543-67de7c8ee0f4.jpg?v=1732287093',
-      link: '/collections/gaming-monitors',
+      link: '/collections/gaming-monitors', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/lenses.jpg?v=1732289718',
-      link: '/collections/camera-lenses',
+      link: '/collections/camera-lenses', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/action.jpg?v=1732289718',
-      link: '/collections/action-cameras',
+      link: '/collections/action-cameras', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/govee-rgb.jpg?v=1732288379',
-      link: '/collections/lighting',
+      link: '/collections/lighting', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/dyson-vacuums.jpg?v=1732288379',
-      link: '/collections/vacuum-cleaners',
+      link: '/collections/vacuum-cleaners', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/streaming.jpg?v=1732289074',
-      link: '/collections/streaming-devices',
+      link: '/collections/streaming-devices', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/smart-home.jpg?v=1732289074',
-      link: '/collections/smart-devices',
+      link: '/collections/smart-devices', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/apple-products_29a11658-9601-44a9-b13a-9a52c10013be.jpg?v=1728311525',
-      link: '/collections/apple-products',
+      link: '/collections/apple-products', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/APPLE-IPHONE-16-wh.jpg?v=1728307748',
-      link: '/collections/apple-iphone',
+      link: '/collections/apple-iphone', // Add link
     },
     {
       src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/ps5-banner.jpg?v=1728289818',
-      link: '/collections/playstation',
+      link: '/collections/playstation', // Add link
     },
+    {
+      src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/apple-products_29a11658-9601-44a9-b13a-9a52c10013be.jpg?v=1728311525',
+      link: '/collections/apple-products', // Add link
+    },
+    {
+      src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/APPLE-IPHONE-16-wh.jpg?v=1728307748',
+      link: '/collections/apple-iphone', // Add link
+    },
+    {
+      src: 'https://cdn.shopify.com/s/files/1/0552/0883/7292/files/ps5-banner.jpg?v=1728289818',
+      link: '/collections/playstation', // Add link
+    },
+    
   ];
 
   const newArrivalsCollection = collections.find((collection) => collection.handle === "new-arrivals");
@@ -210,7 +222,7 @@ export default function Homepage() {
   return (
     <div className="home">
       <BannerSlideshow banners={banners} />
-      <CategorySlider menu={menu} sliderCollections={collections} /> {/* Pass collections as sliderCollections */}
+      <CategorySlider menu={menu} sliderCollections={sliderCollections} /> {/* Pass sliderCollections */}
       <div className="collections-container">
         <>
           {/* Render "New Arrivals" and "Laptops" rows at the start */}
@@ -240,79 +252,79 @@ function DeferredBrandSection({ brands }) {
 
 const GET_COLLECTION_BY_HANDLE_QUERY = `#graphql
   query GetCollectionByHandle($handle: String!) {
-      collectionByHandle(handle: $handle) {
-        id
-        title
-        handle
+    collectionByHandle(handle: $handle) {
+      id
+      title
+      handle
       image {
-          url
-          altText
-        }
-        products(first: 15) {
+        url
+        altText
+      }
+      products(first: 15) {
         nodes {
-            id
-            title
-            handle
+          id
+          title
+          handle
           priceRange {
             minVariantPrice {
-                amount
-                currencyCode
-              }
+              amount
+              currencyCode
             }
+          }
           compareAtPriceRange {
             minVariantPrice {
+              amount
+              currencyCode
+            }
+          }
+          images(first: 1) {
+            nodes {
+              url
+              altText
+            }
+          }
+          variants(first: 5) {
+            nodes {
+              id
+              availableForSale
+              price {
                 amount
                 currencyCode
               }
-            }
-            images(first: 1) {
-            nodes {
-                url
-                altText
-              }
-            }
-            variants(first: 5) {
-            nodes {
-                id
-                availableForSale
-              price {
-                  amount
-                  currencyCode
-                }
               compareAtPrice {
-                  amount
-                  currencyCode
-                }
+                amount
+                currencyCode
+              }
               selectedOptions {
-                  name
-                  value
-                }
+                name
+                value
               }
             }
           }
         }
       }
     }
-      `;
+  }
+`;
 
 export const GET_MENU_QUERY = `#graphql
   query GetMenu($handle: String!) {
-      menu(handle: $handle) {
+    menu(handle: $handle) {
       items {
+        id
+        title
+        url
+        items {
           id
           title
           url
-        items {
+          items {
             id
             title
             url
-          items {
-              id
-              title
-              url
-            }
           }
         }
       }
     }
-      `;
+  }
+`;
