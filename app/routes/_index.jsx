6e8a62da20deb70f745bdf26603ac46b9e70de56
Jad Assaf <jad.a.assaf@gmail.com>
2024-11-28 12:33:41 +0200

    Test

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, useState } from 'react';
 import { defer } from '@shopify/remix-oxygen';
 import { useLoaderData } from '@remix-run/react';
 import { BannerSlideshow } from '../components/BannerSlideshow';
