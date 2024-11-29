7dc41f66314f6f935b7ffa69cb33ef5983bfd1a7
Jad Assaf <jad.a.assaf@gmail.com>
2024-11-29 13:29:54 +0200

    Test

--- app/components/ExpandableMenu.jsx
+++ app/components/ExpandableMenu.jsx
@@ -64,12 +64,7 @@ const ExpandableMenuItem = ({ item, index, isExpanded, isCollapsing, onCategoryC
             dropdown.style.left = "100%";
         } else if ([2, 5, 8].includes(index)) {
             // 3rd, 6th, 9th: Align to the left
-        } else if ([2, 5, 8].includes(index)) {
             dropdown.style.left = "-100%";
-        } else if ([9].includes(index)) {
-            dropdown.style.left = "50%";
-        } else if ([10].includes(index)) {
-            dropdown.style.left = "-50%";
         } else {
             // Others: Default alignment
             dropdown.style.left = "0";
