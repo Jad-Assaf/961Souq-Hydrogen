9ed425d1fe810e420b7950e5fd3a16df610707fe
Jad Assaf <jad.a.assaf@gmail.com>
2024-12-04 14:06:11 +0200

    Test

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -316,7 +316,7 @@ export function Header({ header, isLoggedIn, cart, publicStoreDomain }) {
                           className="menu-item-image"
                         />
                       )}
-                      {item.title} <span className="mobile-menu-arrow">›</span>
+                      {item.title} <span className="mobile-menu-arrow"><svg fill="#2172af" height="24px" width="24px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" stroke="#2172af"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <g> <path d="M256,0C114.618,0,0,114.618,0,256s114.618,256,256,256s256-114.618,256-256S397.382,0,256,0z M256,469.333 c-117.818,0-213.333-95.515-213.333-213.333S138.182,42.667,256,42.667S469.333,138.182,469.333,256S373.818,469.333,256,469.333 z"></path> <path d="M228.418,134.248c-8.331-8.331-21.839-8.331-30.17,0c-8.331,8.331-8.331,21.839,0,30.17L289.83,256l-91.582,91.582 c-8.331,8.331-8.331,21.839,0,30.17c8.331,8.331,21.839,8.331,30.17,0l106.667-106.667c8.331-8.331,8.331-21.839,0-30.17 L228.418,134.248z"></path> </g> </g> </g> </g></svg></span>
                     </button>
                   </div>
                 );
