2f56e322364e00b86461dd09e20cd5b7b0be840e
Jad Assaf <jad.a.assaf@gmail.com>
2024-12-04 12:35:14 +0200

    Test

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -278,64 +278,84 @@ export function Header({ header, isLoggedIn, cart, publicStoreDomain }) {
         </div>
       </header>
 
-      {isMobileMenuOpen && (
-        <div className="mobile-menu-overlay">
-          <button className="mobile-menu-close" onClick={closeMobileMenu}>
-            ✕
-          </button>
-          <h3>Menu</h3>
-          <div className={`mobile-menu-content ${activeSubmenu ? 'hidden' : ''}`}>
-            {menu.items.map((item) => {
-              const image = menuImages.find((img) => img.id === item.id);
-              return (
-                <div key={item.id} className="mobile-menu-item">
-                  <button onClick={() => openSubmenu(item.id)}>
-                    {image && (
-                      <img
-                        src={image.url}
-                        alt={image.alt || item.title}
-                        className="menu-item-image"
-                      />
-                    )}
-                    {item.title} <span className="mobile-menu-arrow">›</span>
-                  </button>
-                </div>
-              );
-            })}
-          </div>
+      <>
+        {/* Overlay for Mobile Menu */}
+        {isMobileMenuOpen && (
+          <div
+            className="mobile-menu-backdrop"
+            onClick={closeMobileMenu} // Close menu when backdrop is clicked
+          ></div>
+        )}
 
-          {activeSubmenu && (
-            <div className="mobile-submenu-drawer" data-id={activeSubmenu}>
-              <button className="back-button" onClick={closeSubmenu}>
-                ‹ Back
-              </button>
-              <div className="submenu-list">
-                {menu.items
-                  .find((item) => item.id === activeSubmenu)
-                  ?.items.map((subItem) => {
-                    const image = menuImages.find((img) => img.id === subItem.id);
-                    return (
-                      <NavLink
-                        key={subItem.id}
-                        to={new URL(subItem.url).pathname}
-                        onClick={closeMobileMenu}
-                      >
-                        {image && (
-                          <img
-                            src={image.url}
-                            alt={image.alt || subItem.title}
-                            className="submenu-item-image"
-                          />
-                        )}
-                        {subItem.title}
-                      </NavLink>
-                    );
-                  })}
-              </div>
+        {/* Mobile Menu */}
+        {isMobileMenuOpen && (
+          <div className="mobile-menu-overlay">
+            <button className="mobile-menu-close" onClick={closeMobileMenu}>
+              <svg
+                fill="#2172af"
+                height="12px"
+                width="12px"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 460.775 460.775"
+              >
+                <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"></path>
+              </svg>
+            </button>
+            <h3>Menu</h3>
+            <div className={`mobile-menu-content ${activeSubmenu ? 'hidden' : ''}`}>
+              {menu.items.map((item) => {
+                const image = menuImages.find((img) => img.id === item.id);
+                return (
+                  <div key={item.id} className="mobile-menu-item">
+                    <button onClick={() => openSubmenu(item.id)}>
+                      {image && (
+                        <img
+                          src={image.url}
+                          alt={image.alt || item.title}
+                          className="menu-item-image"
+                        />
+                      )}
+                      {item.title} <span className="mobile-menu-arrow">›</span>
+                    </button>
+                  </div>
+                );
+              })}
             </div>
-          )}
-        </div>
-      )}
+
+            {activeSubmenu && (
+              <div className="mobile-submenu-drawer" data-id={activeSubmenu}>
+                <button className="back-button" onClick={closeSubmenu}>
+                  ‹ Back
+                </button>
+                <div className="submenu-list">
+                  {menu.items
+                    .find((item) => item.id === activeSubmenu)
+                    ?.items.map((subItem) => {
+                      const image = menuImages.find((img) => img.id === subItem.id);
+                      return (
+                        <NavLink
+                          key={subItem.id}
+                          to={new URL(subItem.url).pathname}
+                          onClick={closeMobileMenu}
+                        >
+                          {image && (
+                            <img
+                              src={image.url}
+                              alt={image.alt || subItem.title}
+                              className="submenu-item-image"
+                            />
+                          )}
+                          {subItem.title}
+                        </NavLink>
+                      );
+                    })}
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </>
     </>
   );
 }
