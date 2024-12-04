760fefba776c599bfc506f1e89215a5f963d97cf
Jad Assaf <jad.a.assaf@gmail.com>
2024-12-04 14:00:27 +0200

    Test

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -80,7 +80,7 @@ export function Header({ header, isLoggedIn, cart, publicStoreDomain }) {
       <header className="header">
         <div className="header-top">
           <button className="mobile-menu-toggle" onClick={toggleMobileMenu}>
-            ☰
+            <svg width="64px" height="64px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#2172af"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5 6H12H19M5 12H19M5 18H19" stroke="#2172af" stroke-width="2" stroke-linecap="round"></path> </g></svg>
           </button>
 
           <NavLink prefetch="intent" to="/" className="logo-link" end>
