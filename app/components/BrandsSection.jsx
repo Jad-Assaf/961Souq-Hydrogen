85df4a228f479df2dbf6a8db86f4dd5d5251578e
Jad Assaf <jad.a.assaf@gmail.com>
2024-12-05 18:45:27 +0200

    Test

--- app/components/BrandsSection.jsx
+++ app/components/BrandsSection.jsx
@@ -5,22 +5,22 @@ import "../styles/BrandsSection.css";
 export const BrandSection = ({ brands }) => {
     return (
         <section className="brand-section">
-                <div className="brand-grid">
-                    <h3>Shop By Brand</h3>
-                    {brands.map((brand, index) => (
-                        <a key={index} href={brand.link} className="brand-item">
-                            <Image
-                                data={{
-                                    altText: brand.name, // Use the brand name as alt text
-                                    url: brand.image,    // URL of the brand image
-                                }}
-                                width="150px" // Set a reasonable width for brand logos
-                                height="auto" // Set a reasonable height for brand logos
-                                sizes="(min-width: 45em) 10vw, 20vw" // Responsive sizes
-                            />
-                        </a>
-                    ))}
-                </div>
+            <h3>Shop By Brand</h3>
+            <div className="brand-grid">
+                {brands.map((brand, index) => (
+                    <a key={index} href={brand.link} className="brand-item">
+                        <Image
+                            data={{
+                                altText: brand.name, // Use the brand name as alt text
+                                url: brand.image,    // URL of the brand image
+                            }}
+                            width="150px" // Set a reasonable width for brand logos
+                            height="auto" // Set a reasonable height for brand logos
+                            sizes="(min-width: 45em) 10vw, 20vw" // Responsive sizes
+                        />
+                    </a>
+                ))}
+            </div>
         </section>
     );
 };
