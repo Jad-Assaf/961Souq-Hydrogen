1636278ffea71e585916a8edd6007c864cdd2433
Jad Assaf <jad.a.assaf@gmail.com>
2024-12-06 16:58:22 +0200

    Test

--- app/components/CollectionSlider.jsx
+++ app/components/CollectionSlider.jsx
@@ -53,7 +53,7 @@ function CategoryItem({ collection, index }) {
                     animate={isInView ? { filter: 'blur(0px)', opacity: 1 } : {}}
                     transition={{ duration: 0.5 }}
                     className="category-svg-wrapper"
-                    style={{ width: '40px', height: '40px' }} // Ensure consistent size
+                    style={{ width: '75px', height: '75px' }} // Ensure consistent size
                 >
                     <div
                         dangerouslySetInnerHTML={{ __html: svgs[index % svgs.length] }}
