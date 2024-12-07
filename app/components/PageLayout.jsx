7d2f700cf5847b595ffc29aa72cdff4f695c258c
Jad Assaf <jad.a.assaf@gmail.com>
2024-12-07 12:20:45 +0200

    Test

--- app/components/PageLayout.jsx
+++ app/components/PageLayout.jsx
@@ -83,95 +83,95 @@ function CartAside({cart}) {
   );
 }
 
-// function SearchAside() {
-//   const queriesDatalistId = useId();
-//   return (
-//     <Aside type="search" heading="SEARCH">
-//       <div className="predictive-search">
-//         <br />
-//         <SearchFormPredictive>
-//           {({ fetchResults, goToSearch, inputRef }) => (
-//             <>
-//               <input
-//                 name="q"
-//                 onChange={fetchResults}
-//                 onFocus={fetchResults}
-//                 placeholder="Search"
-//                 ref={inputRef}
-//                 type="search"
-//                 list={queriesDatalistId}
-//               />
-//               &nbsp;
-//               <button
-//                 onClick={() => {
-//                   goToSearch(); // Corrected: Added parentheses to invoke goToSearch
-//                   closeSearch();
-//                   setSearchResultsVisible(false);
-//                 }}
-//               >
-//                 Search
-//               </button>
-//             </>
-//           )}
-//         </SearchFormPredictive>
+function SearchAside() {
+  const queriesDatalistId = useId();
+  return (
+    <Aside type="search" heading="SEARCH">
+      <div className="predictive-search">
+        <br />
+        <SearchFormPredictive>
+          {({ fetchResults, goToSearch, inputRef }) => (
+            <>
+              <input
+                name="q"
+                onChange={fetchResults}
+                onFocus={fetchResults}
+                placeholder="Search"
+                ref={inputRef}
+                type="search"
+                list={queriesDatalistId}
+              />
+              &nbsp;
+              <button
+                onClick={() => {
+                  goToSearch(); // Corrected: Added parentheses to invoke goToSearch
+                  closeSearch();
+                  setSearchResultsVisible(false);
+                }}
+              >
+                Search
+              </button>
+            </>
+          )}
+        </SearchFormPredictive>
 
-//         <SearchResultsPredictive>
-//           {({items, total, term, state, closeSearch}) => {
-//             const {articles, collections, pages, products, queries} = items;
+        <SearchResultsPredictive>
+          {({items, total, term, state, closeSearch}) => {
+            const {articles, collections, pages, products, queries} = items;
 
-//             if (state === 'loading' && term.current) {
-//               return <div>Loading...</div>;
-//             }
+            if (state === 'loading' && term.current) {
+              return <div>Loading...</div>;
+            }
 
-//             if (!total) {
-//               return <SearchResultsPredictive.Empty term={term} />;
-//             }
+            if (!total) {
+              return <SearchResultsPredictive.Empty term={term} />;
+            }
 
-//             return (
-//               <>
-//                 <SearchResultsPredictive.Queries
-//                   queries={queries}
-//                   queriesDatalistId={queriesDatalistId}
-//                 />
-//                 <SearchResultsPredictive.Products
-//                   products={products}
-//                   closeSearch={closeSearch}
-//                   term={term}
-//                 />
-//                 <SearchResultsPredictive.Collections
-//                   collections={collections}
-//                   closeSearch={closeSearch}
-//                   term={term}
-//                 />
-//                 <SearchResultsPredictive.Pages
-//                   pages={pages}
-//                   closeSearch={closeSearch}
-//                   term={term}
-//                 />
-//                 <SearchResultsPredictive.Articles
-//                   articles={articles}
-//                   closeSearch={closeSearch}
-//                   term={term}
-//                 />
-//                 {term.current && total ? (
-//                   <Link
-//                     onClick={closeSearch}
-//                     to={`${SEARCH_ENDPOINT}?q=${term.current}`}
-//                   >
-//                     <p>
-//                       View all results for <q>{term.current}</q>
-//                       &nbsp; →
-//                     </p>
-//                   </Link>
-//                 ) : null}
-//               </>
-//             );
-//           }}
-//         </SearchResultsPredictive>
-//       </div>
-//     </Aside>
-//   );
-// }
+            return (
+              <>
+                <SearchResultsPredictive.Queries
+                  queries={queries}
+                  queriesDatalistId={queriesDatalistId}
+                />
+                <SearchResultsPredictive.Products
+                  products={products}
+                  closeSearch={closeSearch}
+                  term={term}
+                />
+                <SearchResultsPredictive.Collections
+                  collections={collections}
+                  closeSearch={closeSearch}
+                  term={term}
+                />
+                <SearchResultsPredictive.Pages
+                  pages={pages}
+                  closeSearch={closeSearch}
+                  term={term}
+                />
+                <SearchResultsPredictive.Articles
+                  articles={articles}
+                  closeSearch={closeSearch}
+                  term={term}
+                />
+                {term.current && total ? (
+                  <Link
+                    onClick={closeSearch}
+                    to={`${SEARCH_ENDPOINT}?q=${term.current}`}
+                  >
+                    <p>
+                      View all results for <q>{term.current}</q>
+                      &nbsp; →
+                    </p>
+                  </Link>
+                ) : null}
+              </>
+            );
+          }}
+        </SearchResultsPredictive>
+      </div>
+    </Aside>
+  );
+}
 
 /**
  * @param {{
