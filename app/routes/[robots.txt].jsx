8c25d5d538df109096a4c75e4a0c2c341470df48
Jad-Assaf <jad.a.assaf@gmail.com>
2025-05-19 15:47:48 +0300

    Add to cart button

--- app/routes/[robots.txt].jsx
+++ app/routes/[robots.txt].jsx
@@ -74,7 +74,8 @@ function generalDisallowRules({shopId, sitemapUrl}) {
   return `Disallow: /admin
 Disallow: /facebookConversions
 Disallow: /collections/*/*.json
-Disallow: /collections?direction=*
+Disallow: /collections?direction=previous
+allow: /collections?direction=next&*
 Disallow: /cdn/fonts
 Disallow: /cart
 Disallow: /orders
