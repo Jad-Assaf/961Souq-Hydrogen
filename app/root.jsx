ea57d3434e8de8f6b2f0ea6d85a6316914c55da7
Jad-Assaf <jad.a.assaf@gmail.com>
2025-07-02 12:49:31 +0300

    montserrat

--- app/root.jsx
+++ app/root.jsx
@@ -1,6 +1,6 @@
 // src/root.jsx
 import appStyles from '~/styles/app.css?url';
-// import fontStyles from '../public/fonts.css';
+import fontStyles from '~/styles/fonts.css?url';
 import favicon from '~/assets/961souqLogo_Cart_19e9e372-5859-44c9-8915-11b81ed78213.png';
 import {useNonce, getShopAnalytics, Analytics} from '@shopify/hydrogen';
 import {redirect} from '@shopify/remix-oxygen';
@@ -54,6 +54,10 @@ export function links() {
     // {rel: 'stylesheet', href: productImgStyles},
     // {rel: 'stylesheet', href: searchStyles},
     // {rel: 'stylesheet', href: tailwindCss},
+    {
+      rel: 'stylesheet',
+      href: fontStyles
+    },
     {rel: 'preconnect', href: 'https://cdn.shopify.com'},
     {rel: 'preconnect', href: 'https://shop.app'},
     {rel: 'icon', type: 'image/svg+xml', href: favicon},
@@ -273,7 +277,7 @@ export function Layout({children}) {
           content="width=device-width,initial-scale=1,viewport-fit=cover"
         />
         <link rel="stylesheet" href={appStyles}></link>
-        <link rel="stylesheet" href='/public/fonts.css'></link>
+        {/* <link rel="stylesheet" href={fontStyles}></link> */}
         <Meta />
         <Links />
         {nonce ? (
