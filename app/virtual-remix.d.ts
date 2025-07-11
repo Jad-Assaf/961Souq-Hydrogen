d04a07c74642e73d61c852c96a67f2a371afd5f3
Jad-Assaf <jad.a.assaf@gmail.com>
2025-07-11 15:39:24 +0300

    Country Blocker

--- app/virtual-remix.d.ts
+++
@@ -1,7 +0,0 @@
-// virtual-remix.d.ts
-declare module 'virtual:remix/server-build' {
-    import type { ServerBuild } from '@remix-run/server-runtime';
-    const build: ServerBuild;
-    export = build;
-}
-  
