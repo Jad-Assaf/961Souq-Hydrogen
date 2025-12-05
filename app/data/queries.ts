28749a01cf863316cb78bb7d360e7e7ecc527562
Jad-Assaf <jad.a.assaf@gmail.com>
2025-12-05 14:10:14 +0200

    quick view

--- app/data/queries.ts
+++ app/data/queries.ts
@@ -458,7 +458,7 @@ export const GET_COLLECTION_BY_HANDLE_QUERY = `#graphql
               currencyCode
             }
           }
-          images(first: 10) {
+          images(first: 5) {
             nodes {
               url
               altText
