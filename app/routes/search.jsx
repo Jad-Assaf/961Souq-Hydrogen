9c14de810da2b909e8341746d83a32d47fb4ff3b
Jad-Assaf <jad.a.assaf@gmail.com>
2026-01-02 10:42:35 +0200

    search update

--- app/routes/search.jsx
+++ app/routes/search.jsx
@@ -63,6 +63,7 @@ export async function loader({request, context}) {
     prioritize_num_matching_fields: true,
     text_match_type: 'max_score',
     highlight_full_fields: 'title',
+    filter_by: 'status:=active',
   };
 
   try {
