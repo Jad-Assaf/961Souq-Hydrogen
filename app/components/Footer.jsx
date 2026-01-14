27f964b882338281b79929c2fc84aac42d9f44ef
Jad A. <73811148+Jad-Assaf@users.noreply.github.com>
2026-01-14 18:12:00 +0200

    71888036

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -86,11 +86,11 @@ export const Footer = ({ shopMenu, policiesMenu }) => {
                     </svg>
                   </i>{' '}
                   <a
-                    href="https://wa.me/96181960961"
+                    href="https://wa.me/96171888036"
                     target="_blank"
                     aria-label="Whatsapp Link"
                   >
-                    +961 81 960 961
+                    +961 71 888 036
                   </a>
                 </li> */}
                 <li>
@@ -105,7 +105,7 @@ export const Footer = ({ shopMenu, policiesMenu }) => {
                     </svg>
                   </i>{' '}
                   <a
-                    href="https://wa.me/96181960961"
+                    href="https://wa.me/96171888036"
                     target="_blank"
                     aria-label="Whatsapp Link"
                   >
