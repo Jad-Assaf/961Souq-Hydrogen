155ad64571edb503a1e08496b6ef6475e03de9fa
Jad-Assaf <jad.a.assaf@gmail.com>
2026-01-15 17:00:36 +0200

    03963961

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -86,11 +86,11 @@ export const Footer = ({ shopMenu, policiesMenu }) => {
                     </svg>
                   </i>{' '}
                   <a
-                    href="https://wa.me/96171888036"
+                    href="https://wa.me/9613963961"
                     target="_blank"
                     aria-label="Whatsapp Link"
                   >
-                    +961 71 888 036
+                    +961 03 963 961
                   </a>
                 </li> */}
                 <li>
@@ -105,7 +105,7 @@ export const Footer = ({ shopMenu, policiesMenu }) => {
                     </svg>
                   </i>{' '}
                   <a
-                    href="https://wa.me/96171888036"
+                    href="https://wa.me/9613963961"
                     target="_blank"
                     aria-label="Whatsapp Link"
                   >
