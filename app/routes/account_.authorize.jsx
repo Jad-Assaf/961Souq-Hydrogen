feee5db59533a5b1964c3fa32b209970c1c2e2a5
Jad-Assaf <jad.a.assaf@gmail.com>
2026-01-30 11:07:04 +0200

    customer login fix

--- app/routes/account_.authorize.jsx
+++ app/routes/account_.authorize.jsx
@@ -2,7 +2,27 @@
  * @param {LoaderFunctionArgs}
  */
 export async function loader({context}) {
-  return context.customerAccount.authorize();
+  try {
+    const response = await context.customerAccount.authorize();
+    return withNoStore(response);
+  } catch (error) {
+    if (error instanceof Response) return withNoStore(error);
+    throw error;
+  }
+}
+
+function withNoStore(response) {
+  if (!(response instanceof Response)) return response;
+
+  const headers = new Headers(response.headers);
+  headers.set('Cache-Control', 'no-store');
+  headers.set('Pragma', 'no-cache');
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers,
+  });
 }
 
 /** @typedef {import('@shopify/remix-oxygen').LoaderFunctionArgs} LoaderFunctionArgs */
--- app/routes/account_.login.jsx
+++ app/routes/account_.login.jsx
@@ -2,7 +2,22 @@
  * @param {LoaderFunctionArgs}
  */
 export async function loader({request, context}) {
-  return context.customerAccount.login();
+  const response = await context.customerAccount.login();
+  return withNoStore(response);
+}
+
+function withNoStore(response) {
+  if (!(response instanceof Response)) return response;
+
+  const headers = new Headers(response.headers);
+  headers.set('Cache-Control', 'no-store');
+  headers.set('Pragma', 'no-cache');
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers,
+  });
 }
 
 /** @typedef {import('@shopify/remix-oxygen').LoaderFunctionArgs} LoaderFunctionArgs */
