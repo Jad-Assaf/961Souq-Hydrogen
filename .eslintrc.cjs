df3c00ade2a13f48ccb405ca4a4651c145ab8dba
Jad-Assaf <jad.a.assaf@gmail.com>
2026-03-30 11:27:25 +0300

    2025.7.1

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
   extends: ['@remix-run/eslint-config', 'plugin:hydrogen/recommended'],
+  rules: {
+    'import/no-unresolved': ['error', {ignore: ['^virtual:', '^~/']}],
+  },
 };
